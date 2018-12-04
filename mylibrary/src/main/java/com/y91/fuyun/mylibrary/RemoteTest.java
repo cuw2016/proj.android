05977cf421c734e987e1755b7040909c3188a6fd
cui <1922296263@qq.com>
2018-12-04 14:40:54 +0800

    sdsf

---
+++ mylibrary/src/main/java/com/y91/fuyun/mylibrary/RemoteTest.java
@@ -0,0 +1,11 @@
+package com.y91.fuyun.mylibrary;
+
+/**
+ * Created by cuizhenwei on 2018/12/4.
+ * @author czw
+ */
+public class RemoteTest {
+    public String getTestString() {
+        return "我的库";
+    }
+}
